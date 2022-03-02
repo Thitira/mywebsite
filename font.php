a4301bf616204b1a5a49f7e23a6fb380fbbcfc77
Thitira <97937737+Thitira@users.noreply.github.com>
2022-03-02 09:45:10 +0700

    Update font.php

--- font.php
+++ font.php
@@ -1,30 +1,25 @@
-<!DOCTYPE html>
 <html>
-<head>
-	<meta charset="utf-8">
-	<title>กำหนดรูปแบบตัวอักษร</title>
-  <style>
-    body{
-      background-color: #DEB887;
-    }
-    @font-face{
-      font-family: myFont1;
-      src: url(MiPancake.ttf)
-    }
-     @font-face{
-      font-family: myFont2;
-      src: url(SOVc_PhaKhawMa.ttf)
-    }
-    #font1{
-      font-family: myFont1;
-    }
-    #font2{
-      font-family: myFont2;
-    }
-  </style>
-</head>
-<body>
-  <h1 id="font1">Thitirat Onsri M.4/13 26 58526</h1>
-  <h1 id="font2">Thitirat Onsri M.4/13 26 58526</h1>
-</body>
+  <head>
+    <title>การกำหนดรูปแบบตัวอักษร</title>
+    <style>
+      @font-face{
+        font-family: "MyFont1";
+        src: url("MiPancake.ttf");
+      }
+      @font-face{
+        font-family: "MyFont2";
+        src: url("SOVc_PhaKhawMa.ttf");
+      }
+      #font1{
+        font-family: "MyFont1";
+      }
+      #font2{
+        font-family: "MyFont2";
+      }
+    </style>
+  </head>
+  <body>
+    <h1 id="font1">นางสาว​ฐิติ​รัตน์​ อ่อน​ศรี​ เลขที่​ 26 ห้อง ม4/13 เลขประจำตัว 58526</h1>
+    <h1 id="font2">นางสาว​ฐิติ​รัตน์​ อ่อน​ศรี​ เลขที่​ 26 ห้อง ม4/13 เลขประจำตัว 58526</h1>
+  </body>
 </html>
